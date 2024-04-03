e5f563180aa1a051d10b8c04bda8ac20e12935ea
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 09:40:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function shortcut(s1, s2) {
 	if (s1.length === 0 || s2.length === 0) {
 		return '';
 	}
-	return s1[0].toUpperCase() + s2[0].toUpperCase();
+	return s1[0] + s2[0];
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
